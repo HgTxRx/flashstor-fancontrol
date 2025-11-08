ee111f1625e0065db3d61c1db4861a77663dbb80
HgTxRx <ed@hgtxrx.com>
2025-11-07 22:35:15 -0800

    Update check script for Proxmox compatibility
    Updated script for compatibility with Proxmox and modified repository URL.

--- check_asustor_it87.kmod.sh
+++ check_asustor_it87.kmod.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 : <<'EOF' #Introductory comment block:
-Asustor Flashstor kernel module check/compile script for TrueNAS-SCALE
+Asustor Flashstor kernel module check/compile script for Proxmox 9
 Checks to see if the necessary it87 kmod exists, installs it if not, and runs the fan control script
 By Bernard Mc Clement, Sept 2023. 
-Updated 10/2024 to support Dragonfish
+Converted to Proxmox 11/2025
 
-Note that you can check if the asustor-it87 kmod is installed by running the following command in the truenas shell:
+Note that you can check if the asustor-it87 kmod is installed by running the following command in the Proxmox shell:
  if lsmod | grep -q asustor_it87; then
     echo "asustor-it87 kmod is already installed."
   else
@@ -22,17 +22,17 @@ if ! lsmod | grep -q asustor_it87; then
     echo "asustor-it87 kmod not found or not installed. Compiling and installing..."
 
     # Clone the repository
-    git clone https://github.com/mafredri/asustor-platform-driver
+    git clone https://github.com/hgtxrx/asustor-platform-driver
     cd asustor-platform-driver
 
     # Install dkms
-    sudo apt install -y dkms
+    apt install -y dkms
 
     # Compile the kmod
-    sudo make
+    make
 
     # Install the kmod using dkms
-    sudo make dkms
+    make dkms
 
     echo "asustor-it87 kmod compiled and installed successfully."
 else
@@ -40,4 +40,4 @@ else
 fi
 
 # Run the fan control script
-nohup /home/admin/temp_monitor.sh >/dev/null 2>&1 &
+nohup /root/temp_monitor.sh >/dev/null 2>&1 &
